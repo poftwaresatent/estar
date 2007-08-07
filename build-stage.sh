1680ff6114328bd796ad285821b7ade1586749fc
poftwaresatent <poftwaresatent@255146f7-950e-0410-bd4e-f0acf59374a5>
2007-08-07 22:36:02 +0000

    more flexible (see --help option)
    git-svn-id: https://estar.svn.sourceforge.net/svnroot/estar/trunk/estar@122 255146f7-950e-0410-bd4e-f0acf59374a5

--- build-stage.sh
+++ build-stage.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
-PREFIX=$PWD/stage
-./bootstrap-buildsystem.sh
-if [ $? -ne 0 ]; then
-    echo "ERROR bootstrap-buildsystem.sh"
-    exit 1
+
+PREFIX="$PWD/stage"
+EXTRA_CFGOPTS=""
+RUN_BOOTSTRAP="yes"
+WORKDIR="build"
+
+while [ ! -z "$1" ]; do
+    case $1 in
+	-h|--help)
+echo 'build-stage.sh command line options:'
+echo '  [-p|--prefix]  <PREFIX>     install prefix ($PREFIX)'
+echo '  [-w|--work]    <DIR>        build work directory ($WORKDIR)'
+echo '  [-b|--boost]   <DIR>        BOOST library install directory'
+echo '  [-d|--debug]                enable debug messages and symbols'
+echo '  [-s|--skipbs]               do not bootstrap build system'
+        exit 0;;
+	-p|--prefix)
+	    PREFIX=$2;
+	    shift; shift; continue;;
+	-w|--work)
+	    WORKDIR=$2;
+	    shift; shift; continue;;
+	-b|--boost)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-boost=$2"
+	    shift; shift; continue;;
+	-d|--debug)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --enable-debug"
+	    shift; continue;;
+	-s|--skipbs)
+	    RUN_BOOTSTRAP="no"
+	    shift; continue;;
+	*)
+	    echo "ERROR unhandled option(s) $*" 1>&2
+	    exit 1;;
+    esac
+done
+
+CFGOPTS="--prefix=$PREFIX $EXTRA_CFGOPTS"
+
+echo "configure options: $CFGOPTS"
+
+if [ $RUN_BOOTSTRAP = "yes" ]; then
+    echo "running bootstrap-buildsystem.sh (takes a while)"
+    ./bootstrap-buildsystem.sh
+    if [ $? -ne 0 ]; then
+	echo "ERROR bootstrap-buildsystem.sh"
+	exit 1
+    fi
 fi
 
-rm -rf build
-mkdir build
-if [ $? -ne 0 ]; then
-    echo "ERROR mkdir build"
-    exit 1
+rm -rf $WORKDIR
+if [ ! -d $WORKDIR ]; then
+    mkdir $WORKDIR
+    if [ $? -ne 0 ]; then
+	echo "ERROR mkdir $WORKDIR"
+	exit 1
+    fi
 fi
 
-cd build
-../configure --prefix=$PREFIX
+cd $WORKDIR
+../configure $CFGOPTS
 if [ $? -ne 0 ]; then
-    echo "ERROR ../configure --prefix=$PREFIX"
+    echo "ERROR ../configure $CFGOPTS"
     exit 1
 fi
 
